ab2e0a0515e841c736c93defe3e67cd0e3d38d52
David Turchetta <davidturchetta@outlook.com>
2022-07-31 10:36:26 -0400

    About section layout started.

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,7 +2,42 @@
 const About = () => {
 
   return (
-    <div>About Page</div>
+    <div className = "About-main">
+      <div className = "About-section-div">
+        <div className = "About-section-nest">
+          
+          <div className = "About-title-div">
+            <h1 className = "About-title">
+              About
+            </h1> 
+          </div>
+
+          <div className = "About-section-p-div">
+
+          <p className = "About-section">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet mauris commodo quis imperdiet. Id semper risus in hendrerit gravida rutrum quisque non. 
+          </p>
+          <p className = "About-section">
+            Tristique senectus et netus et malesuada. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Diam phasellus vestibulum lorem sed risus ultricies tristique. Id faucibus nisl tincidunt eget nullam non nisi est. Fermentum et sollicitudin ac orci. 
+          </p>
+           <p className = "About-section">
+            Tristique senectus et netus et malesuada. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Diam phasellus vestibulum lorem sed risus ultricies tristique. Id faucibus nisl tincidunt eget nullam non nisi est. Fermentum et sollicitudin ac orci. 
+          </p>
+          </div>
+          
+
+          <div className = "About-contact-section">
+            <ul className = "About-contact-links">
+              <li className = "About-contact-link">Email</li>
+              <li className = "About-contact-link">LinkedIn</li>
+              <li className = "About-contact-link">Github</li>
+              <li className = "About-contact-link">Codepen</li>
+            </ul>
+          </div>
+            
+        </div>
+      </div>
+    </div>
   )
 }
 

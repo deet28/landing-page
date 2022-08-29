251abd4cd25d36357885c4a819341c25fb81628a
David Turchetta <davidturchetta@outlook.com>
2022-08-29 15:32:53 -0400

    Fixing styling errors in about section.

--- src/components/about/Tech.js
+++ src/components/about/Tech.js
@@ -1,44 +1,39 @@
 import React from 'react';
 const Tech = () => {
 
-  return (
+return (
     <div className = "About-tech-main">
+      
       <h2 className = "About-tech-title">
         Expertise
       </h2>
-
-      <div className = "About-tech-grid-parent">
-        <div className = "About-tech-grid dev">
-          <span className = "dev-title">
-            General
-          </span>
-          <p>
-            Experienced in functional as well as OOP programming. Have worked in web and mobile app development. Have worked with API's and databases. 
-          </p>
-        </div>
+    <div className = "About-tech-grid-parent">
       
+      <div className = "About-tech-grid dev">
+        <span className = "dev-title">General</span>
+        <p>
+          Experienced in functional as well as OOP programming. Have worked in web and mobile app development, with both client-side and server-side frameworks. Have worked with API's and databases. 
+        </p>
+      </div>
       <div className = "About-tech-grid frame">
-          <span className = "frame-title">
-           Languages
-          </span>
+        <span className = "frame-title">Languages</span>
           <p>
-            Experienced in Javascript, Typescript, HTML, and CSS. 
-            Proficient with React framework, as well as React-Native for mobile app development. Proficient with Redux for managing global state in web applications. Experience with Python and Django framework. 
+            Experienced with Javascript, Typescript, HTML, and CSS. 
+            Proficient with React framework, as well as React-Native for mobile app development. Proficient with Redux for managing global state in web applications. Some experience using Python and Django framework. 
           </p>  
-       </div>  
+      </div>  
+      <div className = "About-tech-grid">
+        <span className = "other-title">Other</span>
+        <p>
+          A degree in Psychology, with a focus on human perception and behavioral systems. Certifications in HTML/CSS, Javascript, React/React-Native, and Python/Django. 
+        </p>  
+      </div>  
 
-       <div className = "About-tech-grid">
-          <span className = "other-title">
-           Other
-          </span>
-          <p>
-            A degree in Psychology, with a focus on human perception and behavioral systems. Certifications in HTML/CSS, Javascript and React/React-Native.
-          </p>  
-       </div>  
     </div>
   </div>
   )
 }
+export default React.memo (Tech)
 
 //https://www.freecodecamp.org/certification/fccc4678536-8929-47a1-ba8b-ec9abbf0bbb3/javascript-algorithms-and-data-structures
 
@@ -46,4 +41,3 @@ const Tech = () => {
 
 //
 
-export default React.memo (Tech)
